96110070ac26569838a338d4c080285cc63ccbb3
Sebastian Carroll <seabass25@hotmail.com>
2019-10-26 14:32:21 +0100

    Added column names - some missing

--- inital_load_filter.py
+++ inital_load_filter.py
@@ -1,7 +1,26 @@
-import pandas as pd 
+import pandas as pd
 
-data = pd.read_csv("pp-complete-top100.csv") 
-# Preview the first 5 lines of the loaded data 
+data = pd.read_csv("pp-complete-top100.csv")
+data.columns = [
+    "id",
+    "price",
+    "sale_date",
+    "postcode",
+    "unknown",
+    "unknown",
+    "unknown",
+    "PAON",
+    "SAON",
+    "street",
+    "locality",
+    "town/city",
+    "district",
+    "county",
+    "unknown",
+    "unknown",
+]
+
+# Preview the first 5 lines of the loaded data
 print(data.head())
 #print( "hey")
-print( "hey")
+print("hey")
